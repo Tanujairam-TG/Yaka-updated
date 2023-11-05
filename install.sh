cd062dccd48b40f74981562bb019fc12cfcce6e8
Tanujairam <88611197+Tanujairam-TG@users.noreply.github.com>
2023-11-05 13:23:02 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,36 @@
-#!/usr/bin/bash
+#!/bin/bash
 
+# Check if root privileges are required
+if [[ $EUID -ne 0 ]]; then
+  echo "Please run this script as root or with sudo privileges."
+  exit 1
+fi
+
+# Update package lists
+echo "Updating package lists..."
+pkg update
+
+# Install dependencies
+echo "Installing dependencies..."
 pkg install imagemagick git nodejs ffmpeg libwebp mc nano yarn
-rm -rf session.json 
-rm -rf node_modules
-yarn
+
+# Remove temporary files
+echo "Removing temporary files..."
+rm -rf session.json
+
+# Remove existing node_modules directory if present
+if [ -d "node_modules" ]; then
+  echo "Removing existing node_modules directory..."
+  rm -rf node_modules
+fi
+
+# Install project dependencies using yarn
+echo "Installing project dependencies using yarn..."
+yarn install
+
+# Start the application
+echo "Starting the application..."
 npm start
-echo "Yaka MD and all of it's Dependencies are Installed Successfully!"
+
+# Display success message
+echo "Yaka MD and all of its dependencies are installed successfully!"
